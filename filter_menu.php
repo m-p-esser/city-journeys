6de0062098b33094c069f9527002a8799f16c671
Marc-Philipp Esser <m-esser@mail.de>
2024-09-23 21:09:57 +0200

    add filter menu page

---
+++ filter_menu.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>City Journeys</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Marc-Philipp Esser">
+    <meta name="content" content="Find your next city travel destination">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/x-icon" href="./images/favicon.png">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <div class="filter-options">
+        <h1>Filters</h1>
+        <div class="filter-hint">Select the filter according to your whishes</div>
+        <div class="region-filter">
+            <div class="filter-title">Region</div>
+            <div class="multi-select">
+                <button class="multi-select-button">🇪🇺 Europe</button>
+                <button class="multi-select-button">🏈 North America</button>
+                <button class="multi-select-button">💃 Latin America</button>
+                <button class="multi-select-button">🎎 Asia</button>
+                <button class="multi-select-button">🐆 Africa</button>
+                <button class="multi-select-button">🤿 Oceania</button>
+                <button class="multi-select-button">🇪🇺 European Union</button>
+            </div>
+        </div>
+        <div class="sports-filter">
+            <div class="filter-title">Sports</div>
+            <div class="multi-select">
+                <button class="multi-select-button">🏄 Surfing</button>
+                <button class="multi-select-button">🎿 Skiing</button>
+                <button class="multi-select-button">🤿 Diving</button>
+            </div>
+        </div>
+        <div class="weather-filter">
+            <div class="filter-title">Weather</div>
+            <div class="multi-select">
+                <button class="multi-select-button">💦 <u><strong>Not</strong></u> humid now</button>
+                <button class="multi-select-button">🌧️ <u><strong>Not</strong></u> rainy now</button>
+            </div>
+        </div>
+        <div class="activity-filter">
+            <div class="filter-title">Activities</div>
+            <div class="multi-select">
+                <button class="multi-select-button">🪩 Good nightlife</button>
+            </div>
+        </div>
+        <div class="filter-title">Nearby</div>
+        <div class="nearby-filter">
+            <ul>
+                <li>Near beach</li>
+            </ul>
+        </div>
+        <div class="filter-title">Safety</div>
+        <div class="safety-filter">
+            <ul>
+                <li>Safe for women</li>
+                <li>Low in racism</li>
+            </ul>
+        </div>
+    </div>
+    </div>
+</body>
+
+<!-- <script></script> -->
+
+</body>
+
+<footer>
+    <!-- Footer -->
+    <!-- <div class="footer">Footer</div> -->
+</footer>
+
+</html>
