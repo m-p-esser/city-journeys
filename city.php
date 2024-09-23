d3158b5fc0fb4f8dddeb90986f597f0f07a8c2ed
Marc-Philipp Esser <m-esser@mail.de>
2024-09-23 21:10:19 +0200

    add placeholder for city page

---
+++ city.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>City Journeys</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Marc-Philipp Esser">
+    <meta name="content" content="Find your next city travel destination">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/x-icon" href="./images/favicon.png">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <p>Is part of Schengen region: yes/no (include hint what Schengen region is)
+    <p>
+
+        <!-- <script></script> -->
+
+</body>
+
+<footer>
+    <!-- Footer -->
+    <!-- <div class="footer">Footer</div> -->
+</footer>
+
+</html>
